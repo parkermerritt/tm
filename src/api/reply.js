39ec95b405ae56294ad3063c476caf0748316bdf
Glitch (harmonious-cream) <none>
2018-03-14 19:13:47 +0000

    😺📹 Checkpoint ./src/api/reply.js:747261/545

--- src/api/reply.js
+++ src/api/reply.js
@@ -34,11 +34,15 @@ const reply = event => {
   }
   const response = randomReply()
   
-  var ticker = client.getTicker({limit: 1, currency: 'bitcoin'}).then(console.log).catch(console.error)
+  var ticker = client.getTicker({limit: 1, currency: 'bitcoin'}).then(console.log).catch(console.error)   
+  
+  ticker.then(function(result) {
+   console.log(result) //will log results.
+})
+  
   var price = ticker.price_usd;
   //console.log(price);
-  ticker = String(ticker);
-  price = String(price);
+  console.log(ticker);
   console.log(price);
   
   var mapObj = {
@@ -46,6 +50,8 @@ const reply = event => {
      price:price,
     '@${screenName}':screenName
   };
+  
+
   
   var res = response.replace(/coin|price|@${screenName}'/gi, function(matched){
   return mapObj[matched];
