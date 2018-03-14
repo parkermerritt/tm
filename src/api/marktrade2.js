aabbbfcdbeb15a1cb1612ef094a43a64ab6ac684
Glitch (harmonious-cream) <none>
2018-03-14 18:07:49 +0000

    🎹🍥 Checkpoint ./src/api/marktrade2.js:747261/2067

--- src/api/marktrade2.js
+++ src/api/marktrade2.js
@@ -1,11 +1,35 @@
 const Twit = require('twit')
 const unique = require('unique-random-array')
 const config = require('../config')
+const CoinMarketCap = require('coinmarketcap-api')
+
 
 const param = config.twitterConfig
 const randomReply = unique(param.randomReply.split('|'))
 
 const bot = new Twit(config.twitterKeys)
+const client = new CoinMarketCap()
+
+
+// CoinMarketCap info search based on coin name selected in getName
+function searchImage (searchName, callback) {
+  var ticker = client.getTicker({limit: 1, currency: 'bitcoin'}).then(console.log).catch(console.error)
+  var price = client.getTicker("bitcoin").price_usd();
+  console.log(price);
+  
+        var coin = searchName;
+        var price = client.getTicker({limit: 1, currency:'bitcoin'}).price_usd;
+        var botData      = {coin, price};
+
+        console.log('*** search was a success! moving the required botData along...');
+        // callback(null, botData);
+        setTimeout(function () {
+          callback(null, botData);
+          // console.log(botData);
+        }, 5000); // create a delay before processing the botData
+      }
+    });
+}
 
 // function: tweets back to user who followed
 function tweetNow(text) {
